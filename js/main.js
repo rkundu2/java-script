03f9ed6c59e58cfb35b721ec332fb2a71915430d
rkundu2 <niloy321123@gmail.com>
2022-01-19 21:09:03 +0600

    first javascript

---
+++ js/main.js
@@ -0,0 +1,62 @@
+let x = 5;
+let y = 4;
+
+let A = x + y;
+document.getElementById("demo").innerHTML =A ;
+
+let B = x-y;
+document.getElementById("demo1").innerHTML = B;
+
+let C = x * y;
+document.getElementById("demo2").innerHTML = C;
+
+let D = x ** y;
+document.getElementById("demo3").innerHTML =D;
+
+let m = 25;
+let n = 5;
+
+let E = m / n;
+document.getElementById("demo4").innerHTML =E;
+
+let o = 21;
+let p = 5;
+
+let F = o % p;
+document.getElementById("demo5").innerHTML =F;
+
+let q = 6;
+
+++q;
+document.getElementById("demo6").innerHTML =q;
+
+let z = 6;
+--z;
+document.getElementById("demo7").innerHTML =z;
+
+let Aa=(x==y);
+document.getElementById("demo8").innerHTML =Aa ;
+
+let Ba=(x!=y);
+document.getElementById("demo9").innerHTML =Ba ;
+
+let Ca=(x>y);
+document.getElementById("demo10").innerHTML =Ca ;
+
+let Da=(x<y);
+document.getElementById("demo11").innerHTML =Da ;
+
+let Ea=(x>=y);
+document.getElementById("demo12").innerHTML =Ea ;
+
+let Fa=(x<=y);
+document.getElementById("demo13").innerHTML =Fa ;
+
+
+
+
+
+
+
+
+
